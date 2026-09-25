04d3eb5d6c857d47472d6a53ead518f958de0057
unknown <joseph.gnk2004@gmail.com>
2026-09-25 19:34:43 +0530

    'First_work'

---
+++ California_app.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import joblib
+import sklearn
+
+
+obj=joblib.load('California.joblib')
+model=obj['Califronia.joblib']
+col=obj['columns']
+
+st.title('California app')
+In=[]
+for i in col:
+    v=st.number_input(f'Enter {i} value=')
+    In.append(v)
+if st.button('click'):
+    out=model.predict([In])  
+    st.success(f'The median House value is :{out}')
+
+# python -m ven ml_linear
